2f03a005473df04d48f0c07ffcee2ad467c2dc8f
ShtylenkoAS <73440850+ShtylenkoAS@users.noreply.github.com>
2022-03-27 17:39:00 +0300

    Задача 2

--- CreateVariable.java
+++ CreateVariable.java
@@ -4,7 +4,7 @@ public class CreateVariable {
     public static void main(String[] args) {
         double pi = 3.14159265359;
         byte var = 127;
-
+        //пробую метод printf
         System.out.printf("Моя произволная переменная с плавающей точкой с наибольшим диапазоном чисел: %f \nМоя произволная переменная целочисленного типа с наименьшим диапазоном чисел: %d \nПрограмма окончена", pi, var);
     }
 }
