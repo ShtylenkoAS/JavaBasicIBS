fbe5e4a7ebe643a64539e0e57295eda88f89fe22
ShtylenkoAS <73440850+ShtylenkoAS@users.noreply.github.com>
2022-03-27 17:38:06 +0300

    Задача 1

--- Welcome.java
+++ Welcome.java
@@ -3,6 +3,6 @@ package com.max.idea.JavaBasicIBS;
 public class Welcome {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        System.out.println("Hello World!");     //вывод в консоль
     }
 }
